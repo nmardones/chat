cc8aa2993b4a77e9453262676a448497f9424055
nmardones <nmardones.fabre.21@gmail.com>
2018-09-06 01:53:17 -0300

    bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@ sudo composer update
 sudo cp env-chat .env
 sudo php artisan key:generate
 sudo php artisan cache:clear 
+sudo chmod -R 777 ../chat2
 cd laradock
 sudo cp env-example .env 
 sudo docker-compose up -d nginx mysql redis
